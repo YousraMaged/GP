dd8a745f142a259b8ddd1e8977cb08a8f9693f87
Sarah Maghraby <Sara@SARA.local>
2018-06-13 13:28:25 +0200

    Payment

--- GeoCity/client/src/app/app.module.ts
+++ GeoCity/client/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -13,11 +14,14 @@ import { ReportsComponent } from './components/reports/reports.component';
 import { RequestsComponent } from './components/requests/requests.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { BusComponent } from './components/bus/bus.component';
+import { BusService } from './services/bus.service';
+import { PaymentComponent } from './components/payment/payment.component';
 
 const appRoutes: Routes = [
   {path:'', component:HomeComponent},
   {path:'map', component:MapComponent},
-  {path:'transportation', component:BusComponent}
+  {path:'transportation', component:BusComponent},
+  {path:'payment', component:PaymentComponent}
 ]
 
 @NgModule({
@@ -29,16 +33,19 @@ const appRoutes: Routes = [
     ReportsComponent,
     RequestsComponent,
     FooterComponent,
-    BusComponent
+    BusComponent,
+    PaymentComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes),
     HttpModule,
-    FormsModule
+    FormsModule,
+    CarouselModule
   ],
   providers: [
-    MapService
+    MapService,
+    BusService
   ],
   bootstrap: [AppComponent]
 })
--- GeoCity/client/src/app/components/bus/bus.component.ts
+++ GeoCity/client/src/app/components/bus/bus.component.ts
@@ -1,15 +1,58 @@
 import { Component, OnInit } from '@angular/core';
+import { CarouselConfig } from 'ngx-bootstrap/carousel';
+import { BusService } from '../../services/bus.service';
 
 @Component({
   selector: 'app-bus',
   templateUrl: './bus.component.html',
-  styleUrls: ['./bus.component.css']
+  styleUrls: ['./bus.component.css'],
+  providers: [
+    { provide: CarouselConfig, useValue: { interval: 3000, noPause: true, showIndicators: true } }
+  ]
 })
 export class BusComponent implements OnInit {
+  image1: any = "../../../assets/img/Transportation hero.jpg";
+  image2: any = "../../../assets/img/buses.jpg";
+  image3: any = "../../../assets/img/Future-Blog.jpg";
+  image4: any = "../../../assets/img/transportation_main.jpg";
+  user = {
+    origin: '',
+    destination: ''
+  }
+  Route: any;
 
-  constructor() { }
+
+
+
+  constructor(public busService: BusService) {
+    this.busService.getRoute().subscribe(res => {
+      res.json();
+      this.Route = res.json();
+      console.log(this.Route[0].Path);
+    })
+  }
 
   ngOnInit() {
   }
 
+  onSubmit({ value, valid }: { value: any, valid: boolean }) {
+    if (!valid) {
+      console.log("not valid")
+    } else {
+      let origin = value.origin;
+      let destination = value.destination;
+      for (let i = 0; i < this.Route.length; i++) {
+        for (let j = 0; j < this.Route[i].Path.length; j++) {
+          if (origin == this.Route[i].Path[j] && destination == this.Route[i].Path[j]) {
+            console.log(this.Route[i].number)
+          } else {
+            console.log("false")
+          }
+        }
+      }
+      // console.log(origin)
+      // console.log(destination)
+    }
+  }
 }
+
