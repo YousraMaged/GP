919a185feb1f6f01673da5d1662de3df499d9947
Yousra Maged <hyper tech@Yousra.local>
2018-06-14 10:43:04 +0200

    Reports Component

--- GeoCity/client/src/app/components/reports/reports.component.ts
+++ GeoCity/client/src/app/components/reports/reports.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as leaflet from 'leaflet';
+import { map } from 'rxjs/operator/map';
 
 @Component({
   selector: 'app-reports',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportsComponent implements OnInit {
 
-  constructor() { }
+  public Map: any;
+  public BaseMap: any;
+  public markerIcon = require('leaflet/dist/images/marker-icon.png');
+  public markerShadow = require('leaflet/dist/images/marker-shadow.png');
+  private defaultIcon = leaflet.icon({
+    iconUrl: this.markerIcon,
+    shadowUrl: this.markerShadow,
+    iconAnchor: [13, 41]
+  });
+  notify: boolean = false;
+
+
+
+  constructor() {
+  }
 
   ngOnInit() {
+    leaflet.Marker.prototype.options.icon = this.defaultIcon;
+    this.Map = leaflet.map('map').setView([30.142833, 31.626871], 13);
+    this.BaseMap = leaflet.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(this.Map);
+    this.Map.on('click', (e) => {
+      alert(e.latlng.lat + ", " + e.latlng.lng);
+      leaflet.marker([e.latlng.lat, e.latlng.lng]).addTo(this.Map);
+    })
+  }
+
+  toggle_notify() {
+    this.notify = !this.notify;
   }
 
 }
