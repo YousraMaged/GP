ad40f1e065bef9c4917b74d4043eb8a79a17fdde
Sarah Maghraby <Sara@SARA.local>
2018-06-16 10:44:12 +0200

    extended bus function

--- GeoCity/client/src/app/components/bus/bus.component.ts
+++ GeoCity/client/src/app/components/bus/bus.component.ts
@@ -18,17 +18,23 @@ export class BusComponent implements OnInit {
   image3: any = "../../../assets/img/Future-Blog.jpg";
   image4: any = "../../../assets/img/transportation_main.jpg";
   user = {
-    origin: '',
+    origin: 'select',
     destination: ''
   }
-  Route: any;
 
- 
+  
+  origins = [
+    {name: "El-Sherouk first gate"},
+    {name: "El-A'sher bus station"}
+  ]
+  Route: any;
   value: any = null;
+
 
   selectChange(e)
   {
     this.value = e.target.value;
+    console.log(e.target.value);
   }
 
 
@@ -38,9 +44,10 @@ export class BusComponent implements OnInit {
     this.busService.getRoute().subscribe(res => {
       res.json();
       this.Route = res.json();
-      console.log(this.Route[0].Path);
     })
   }
+
+ 
 
   ngOnInit() {
 
@@ -49,24 +56,24 @@ export class BusComponent implements OnInit {
 
 
 
-  // onSubmit({ value, valid }: { value: any, valid: boolean }) {
-  //   if (!valid) {
-  //     console.log("not valid")
-  //   } else {
-  //     let origin = value.origin;
-  //     let destination = value.destination;
-  //     for (let i = 0; i < this.Route.length; i++) {
-  //       for (let j = 0; j < this.Route[i].Path.length; j++) {
-  //         if (origin == this.Route[i].Path[j] && destination == this.Route[i].Path[j]) {
-  //           console.log(this.Route[i].number)
-  //         } else {
-  //           console.log("false")
-  //         }
-  //       }
-  //     }
-  // console.log(origin)
-  // console.log(destination)
-  //   }
-  // }
+  onSubmit({ value, valid }: { value: any, valid: boolean }) {
+    if (!valid) {
+      console.log("not valid")
+    } else {
+      let origin = value.origin;
+      let destination = value.destination;
+      for (let i = 0; i < this.Route.length; i++) {
+        for (let j = 0; j < this.Route[i].Path.length; j++) {
+          if (origin == this.Route[i].Path[j] && destination == this.Route[i].Path[j]) {
+            console.log(this.Route[i].number)
+          } else {
+            console.log("false")
+          }
+        }
+      }
+  console.log(origin)
+  console.log(destination)
+    }
+  }
 
 }
--- GeoCity/common/models/bus.js
+++ GeoCity/common/models/bus.js
@@ -5,32 +5,39 @@ module.exports = function (Bus) {
     Bus.remoteMethod("path", {
         http: { path: "/path", verb: "get" },
         accepts: [{ arg: "origin", type: "string" }, { arg: "destination", type: "string" }],
-        returns: [{ arg: "busNumber", type: "string" }, { arg: "path", type: "object" }]
+        returns: [{ arg: "Stations", type: "object" }, { arg: "Bus Number", type: "string"}]
     });
 
     Bus.path = function (origin, destination, cb) {
         console.log("Remote method working");
-        Bus.findOne({ where: { origin: origin } }, function (err, bus) {
+        Bus.find({fields: { Stations: true }}, function (err, bus) {
             cb(err, bus);
+            bus.filter({"where":{ "and":[{ "origin": {"inq": "Stations"}}, { "destination": {"inq": "Stations"}}] }}, function (err, bus) {
+                cb(err, bus);
+            
+            });
 
-            if (!valid) {
-                console.log("not valid")
-            } else {
-                let origin = value.origin;
-                let destination = value.destination;
-                for (let i = 0; i < this.Route.length; i++) {
-                    for (let j = 0; j < this.Route[i].Path.length; j++) {
-                        if (origin == this.Route[i].Path[j] && destination == this.Route[i].Path[j]) {
-                            console.log(this.Route[i].number)
-                        } else {
-                            console.log("false")
-                        }
-                    }
-                }
-                console.log(origin)
-                console.log(destination)
-            }
+           
+
+            // if (!valid) {
+            //     console.log("not valid")
+            // } else {
+            //     let origin = value.origin;
+            //     let destination = value.destination;
+            //     for (let i = 0; i < this.Route.length; i++) {
+            //         for (let j = 0; j < this.Route[i].Path.length; j++) {
+            //             if (origin == this.Route[i].Path[j] && destination == this.Route[i].Path[j]) {
+            //                 console.log(this.Route[i].number)
+            //             } else {
+            //                 console.log("false")
+            //             }
+            //         }
+            //     }
+            //     console.log(origin)
+            //     console.log(destination)
+            // }
         }
-        );
+        
+    );
     };
 };
